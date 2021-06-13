5288bec5c1c66a9dac1eadefcbfe5daa7c3e681c
elmetal <nm7.pupu@gmail.com>
2021-06-13 12:13:59 +0900

    Initial Commit

---
+++ wwdc2021-playground/wwdc2021_playgroundApp.swift
@@ -0,0 +1,18 @@
+//
+//  wwdc2021_playgroundApp.swift
+//  wwdc2021-playground
+//  
+//  Created by elmetal on 2021/06/13
+//  
+//
+
+import SwiftUI
+
+@main
+struct wwdc2021_playgroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ wwdc2021-playgroundTests/wwdc2021_playgroundTests.swift
@@ -0,0 +1,34 @@
+//
+//  wwdc2021_playgroundTests.swift
+//  wwdc2021-playgroundTests
+//  
+//  Created by elmetal on 2021/06/13
+//  
+//
+
+import XCTest
+@testable import wwdc2021_playground
+
+class wwdc2021_playgroundTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
